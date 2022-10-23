ea7bac06578ce5291af19e2b31d94ba943cd6fc5
Jesuslovesshiva <111922763+Jesuslovesshiva@users.noreply.github.com>
2022-10-23 19:15:54 +0200

    Test2

--- FCC-JS/hello.js
+++ FCC-JS/hello.js
@@ -15,3 +15,5 @@ let testArr = [1, 2, 3, 4, 5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
+
+//Next In Line
