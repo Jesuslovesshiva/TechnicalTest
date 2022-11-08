3ceada4fd5e967241a08053558b4c97255719cea
Jesuslovesshiva <111922763+Jesuslovesshiva@users.noreply.github.com>
2022-11-09 00:16:38 +0100

    fuck

---
+++ udemy_JS_1/ZIPs/src/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Eingabe Daten holen
+let titel = prompt("Titel:");
+console.log(titel);
+let typ = prompt("Typ (Einnahme oder Ausgabe):");
+console.log(typ);
+let betrag = prompt("Betrag (in Cent):");
+console.log(betrag);
+let datum = prompt("Datum (jjjj-mm-tt):");
+console.log(datum);
+
+// Gesamtbilanz
+let einnahmen;
+let ausgaben;
+let bilanz;
+
+
